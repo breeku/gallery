874a8ab504cf2c5fc50fe3e70f985dddee507c70
breeku <bregfi@hotmail.com>
2017-12-10 21:29:45 +0200

    back to top, erasing obsolete code

--- js/script.js
+++ js/script.js
@@ -12,26 +12,24 @@ var $grid = $('.grid').isotope({
 $('.filter-button-group').on( 'click', 'button', function() {
   $grid.isotope({ filter: $(this).attr('data-filter') });
 });
-$('#myCarousel').carousel({
-  interval: 40000
-});
-$('#myCarousel').carousel({
-    interval: 10000
-})
 
-$('.carousel .item').each(function(){
-    var next = $(this).next();
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 60) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
 
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-
-    next.children(':first-child').clone().appendTo($(this));
-
-    if (next.next().length>0) {
-        next.next().children(':first-child').clone().appendTo($(this));
-    }
-    else {
-        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
 });
